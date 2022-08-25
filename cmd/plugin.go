3ef5d3376591b2e0d877705a6d67398856cc6a7c
gabbyprecious <gabbyprecious2000@gmail.com>
2022-08-25 17:57:12 +0100

    Building plugin functionalities

--- cmd/plugin.go
+++ cmd/plugin.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	core "github.com/cln-reckless/cln4go.plugin/pkg/plugin"
+	core "github.com/gabbyprecious/demo-cln-plugin/pkg/plugin"
 
 	"github.com/vincenzopalazzo/cln4go/plugin"
 )
@@ -10,7 +10,12 @@ func main() {
 	state := core.PluginState{}
 	plugin := plugin.New(&state, true, plugin.DummyOnInit[core.PluginState])
 	plugin.RegisterOption("foo", "string", "Hello Go", "An example of option", false)
+	plugin.RegisterOption("demo-email", "string", "foo@email.com", "Email Plugin should send payment email to", false)
+
 	plugin.RegisterRPCMethod("hello", "", "an example of rpc method", &core.Hello[core.PluginState]{})
+	plugin.RegisterRPCMethod("networktester", "", "an example of rpc method", &core.NetworkChecker[core.PluginState]{})
+
+	plugin.RegisterNotification("on-payment", &core.OnPayment[core.PluginState]{})
 	plugin.RegisterNotification("shutdown", &core.OnShutdown[core.PluginState]{})
 	plugin.Start()
 }
--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -1,10 +1,18 @@
 package plugin
 
 import (
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/vincenzopalazzo/cln4go/client"
 
 	"github.com/vincenzopalazzo/cln4go/plugin"
 )
+
+func sendMail(email string) {
+	// send mail or do something else
+}
 
 type PluginState struct{}
 
@@ -12,6 +20,46 @@ type Hello[T PluginState] struct{}
 
 func (instance *Hello[T]) Call(plugin *plugin.Plugin[T], request map[string]any) (map[string]any, error) {
 	return map[string]any{"message": "hello from cln4go.template"}, nil
+}
+
+type NetworkChecker[T PluginState] struct{}
+
+func (instance *NetworkChecker[PluginState]) Call(plugin *plugin.Plugin[PluginState], request map[string]any) (map[string]any, error) {
+	clnPath, found := plugin.GetConf("lightning-dir")
+	if !found {
+		panic(found)
+	}
+
+	rpcFileName, found := plugin.GetConf("rpc-file")
+	if !found {
+		panic(found)
+	}
+	unixPath := strings.Join([]string{clnPath.(string), rpcFileName.(string)}, "/")
+	client, err := client.NewUnix(unixPath)
+	if err != nil {
+		return map[string]any{"message": "error setting up client"}, err
+	}
+	response, err := client.Call("getinfo", make(map[string]interface{}))
+
+	if err != nil {
+		return map[string]any{"message": "error calling getinfo"}, err
+	}
+	network := response["network"]
+	if !found {
+		return map[string]any{"message": "unknown"}, nil
+	}
+	return map[string]any{"message": fmt.Sprintf("running on %s", network.(string))}, nil
+}
+
+type OnPayment[T PluginState] struct{}
+
+func (instance *OnPayment[PluginState]) Call(plugin *plugin.Plugin[PluginState], request map[string]any) {
+	receivingEmail, found := plugin.GetOpt("demo-email")
+
+	if !found {
+		receivingEmail = "bar@email.com"
+	}
+	sendMail(receivingEmail.(string))
 }
 
 type OnShutdown[T PluginState] struct{}
